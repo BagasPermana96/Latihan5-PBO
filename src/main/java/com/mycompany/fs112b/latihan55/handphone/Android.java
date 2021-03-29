1df3c269b445bd5de0f106ace99f11895f42c03d
BagasPermana96 <muhamadbagas96@gmail.com>
2021-03-29 22:52:34 +0700

    Upload Project

---
+++ src/main/java/com/mycompany/fs112b/latihan55/handphone/Android.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fs112b.latihan55.handphone;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Bagas Permana
+ * KELAS    : FS112B-PBO
+ * NIK      : 2022431570
+ * Deskripsi Program	: Program ini berisi program untuk menampilan
+ * Android key store
+ * 
+ */
+public class Android extends Handphone {
+    
+    private String keyStore;
+
+    public Android(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getKeyStore() {
+        return keyStore;
+    }
+
+    public void setKeyStore(String keyStore) {
+        this.keyStore = keyStore;
+    }
+}
---
+++ src/main/java/com/mycompany/fs112b/latihan55/handphone/WindowsPhone.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.fs112b.latihan55.handphone;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Bagas Permana
+ * KELAS    : FS112B-PBO
+ * NIK      : 2022431570
+ * Deskripsi Program	: Program ini berisi program untuk menampilkan
+ * WP key store
+ * 
+ */
+public class WindowsPhone extends Handphone{
+        private String wpKeyStore;
+
+    public WindowsPhone(String man, String os, String model, int harga) {
+        super(man, os, model, harga);
+    }
+
+    public String getWpKeyStore() {
+        return wpKeyStore;
+    }
+
+    public void setWpKeyStore(String wpKeyStore) {
+        this.wpKeyStore = wpKeyStore;
+    }
+}
